b307d2ed72d561e7752107192ee3848ffe0fcacc
ssh68 <610353134@qq.com>
2019-04-21 11:14:52 +0800

    修改后提交

--- a.js
+++ a.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-/*
-* @Author: Administrator
-* @Date:   2019-04-21 09:50:22
-* @Last Modified by:   Administrator
-* @Last Modified time: 2019-04-21 10:01:17
-*/
-
-'use strict';
-
 
 
 bu nishi qiaozhi 
-=======
 woshipeiqi
->>>>>>> apple
+
--- b.js
+++ b.js
@@ -5,4 +5,3 @@ haha
 eihei
 
 wdnjjndvj
-
